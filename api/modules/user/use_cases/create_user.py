ef17dfbe118042f766b7432f607e2e37f0f5d376
MicaelRiboura <micaelribouradossantos@gmail.com>
2023-11-20 15:57:33 -0300

    feat: create create_user use case

---
+++ api/modules/user/use_cases/create_user.py
@@ -0,0 +1,21 @@
+from ..daos.user_dao import UserDAO
+from sqlalchemy.exc import IntegrityError
+
+def create_user(form):
+    try:
+        userDAO = UserDAO()
+
+        user = userDAO.create(form)
+        
+        return user, 200
+
+    except IntegrityError as e:
+        error_msg = "Usuário de mesmo e-mail já salvo na base!"
+        return {"message": error_msg}, 409
+
+    except Exception as e:
+        # caso um erro fora do previsto
+        print('error: ', e)
+        error_msg = "Não foi possível salvar novo usuário :/"
+        # logger.warning(f"Erro ao adicionar produto '{produto.nome}', {error_msg}")
+        return {"message": error_msg}, 400
