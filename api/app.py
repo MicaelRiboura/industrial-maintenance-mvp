ca75a0a49b44e90e7ae5f5907d27ec0c8939c631
MicaelRiboura <micaelribouradossantos@gmail.com>
2023-11-20 22:01:59 -0300

    feat: create equipment routes in app

--- api/app.py
+++ api/app.py
@@ -8,9 +8,19 @@ from flask_cors import CORS
 # schemas
 from modules.shared.errors.error_schema import ErrorSchema
 from modules.user.schemas import UserSchema, UserResponseSchema, UserLoginSchema
+from modules.equipment.schemas import \
+    CreateEquipmentSchema, \
+    EquipmentResponseSchema, \
+    ListEquipmentsQueryByUserSchema, \
+    ListEquipmentsSchema, \
+    DeleteResponseSchema, \
+    DeleteEquipmentSchema
 
 # usecases
 from modules.user.use_cases import create_user, login
+from modules.equipment.use_cases import create_equipment, \
+    list_equipments_by_user, \
+    delete_equipment
 
 
 info = Info(title="API - Maintenance Company", version="1.0.0")
@@ -25,7 +35,6 @@ def home():
     """Redireciona para /openapi, tela que permite a escolha do estilo de documentação.
     """
     return redirect('/openapi')
-
 
 # ----------------------------- Users Routes -----------------------------
 user_tag = Tag(name="Usuário", description="Criação e login de usuários à base de dados.")
@@ -44,4 +53,26 @@ def login_route(form: UserLoginSchema):
     """
     return login(form)
 
+# ----------------------------- Equipments Routes -----------------------------
+equipment_tag = Tag(name="Equipamentos", description="Adição, visualização e deleção de equipamentos à base de dados.")
 
+@app.post('/equipments/create', tags=[equipment_tag], responses={'200': EquipmentResponseSchema, '400': ErrorSchema, '404': ErrorSchema})
+def create_equipment_route(form: CreateEquipmentSchema):
+    """
+        Cria novo equipamento
+    """
+    return create_equipment(form)
+
+@app.get('/equipments/user', tags=[equipment_tag], responses={'200': ListEquipmentsSchema, '404': ErrorSchema})
+def list_equipments_by_user_route(query: ListEquipmentsQueryByUserSchema):
+    """
+        Lista equipamentos de um usuário
+    """
+    return list_equipments_by_user(query)
+
+@app.delete('/equipments/delete', tags=[equipment_tag], responses={'200': DeleteResponseSchema, '404': ErrorSchema})
+def delete_equipment_route(query: DeleteEquipmentSchema):
+    """
+        Remove um equipamento através de seu identificador
+    """
+    return delete_equipment(query)
