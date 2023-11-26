226ba6f324dcc13c0f4f892fcda698b5938d163c
MicaelRiboura <micaelribouradossantos@gmail.com>
2023-11-26 20:38:26 -0300

    feat(front): create header component

---
+++ front/src/components/Header.js
@@ -0,0 +1,13 @@
+import Logo from "../assets/images/logo.svg";
+export default function Header() {
+    return (
+        <header className="bg-dark-800" style={{ height: '80px' }}>
+            <div className="h-full flex items-center justify-between" style={{ margin: 'auto', maxWidth: '1440px' }}>
+                <img src={Logo} alt="RelEquip Logo" className="mb-4" style={{ height: '40px' }} />
+                <div className="flex text-white font-bold">
+                    <span>Usuário</span>
+                </div>
+            </div>
+        </header>
+    );
+}
