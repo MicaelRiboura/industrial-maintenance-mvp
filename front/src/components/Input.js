6db429c7da2c90c9b17e9d741d09da087b8d1eae
MicaelRiboura <micaelribouradossantos@gmail.com>
2023-11-26 20:37:44 -0300

    feat(front): create input component

---
+++ front/src/components/Input.js
@@ -0,0 +1,14 @@
+export default function Input({ name, label, type, placeholder, groupClassName }) {
+    return (
+        <div className={groupClassName}>
+            <label for={name} className="block mb-2 text-sm font-medium text-white">{label}</label>
+            <input
+                id={name}
+                type={type} 
+                name={name}
+                className="w-full bg-dark-800 px-3 py-2 mb-1 border-2 border-dark-800 rounded-md focus:outline-none focus:border-primary-500 transition-colors"
+                placeholder={placeholder}
+            />
+        </div>
+    );
+}
