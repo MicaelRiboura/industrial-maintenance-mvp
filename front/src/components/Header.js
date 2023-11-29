83cc17d6143b1cdd3161e92c6098192788aade91
MicaelRiboura <micaelribouradossantos@gmail.com>
2023-11-29 10:24:04 -0300

    feat(front): add user info and signout in header

--- front/src/components/Header.js
+++ front/src/components/Header.js
@@ -1,12 +1,22 @@
+import { FaUserCircle } from "react-icons/fa";
 import Logo from "../assets/images/logo.svg";
+import { useAuth } from "../contexts/AuthContextApiHook";
+import { FiLogOut } from "react-icons/fi";
+
 export default function Header() {
+    const { signed, user, signOut } = useAuth();
+
     return (
         <header className="bg-dark-800" style={{ height: '80px' }}>
             <div className="h-full flex items-center justify-between" style={{ margin: 'auto', maxWidth: '1440px' }}>
                 <img src={Logo} alt="RelEquip Logo" className="mb-4" style={{ height: '60px' }} />
-                <div className="flex text-white font-bold">
-                    <span>Usuário</span>
-                </div>
+                {signed && (
+                    <div className="flex items-center text-white font-bold">
+                        <FaUserCircle className="text-lg mr-2" />
+                        <span>{user.name}</span>
+                        <FiLogOut className="text-lg ml-4 cursor-pointer" onClick={() => signOut()} />
+                    </div>
+                )}
             </div>
         </header>
     );
