7160e548827c746b527e001e0f8a19f46acc03e4
MicaelRiboura <micaelribouradossantos@gmail.com>
2023-11-29 10:27:17 -0300

    feat(front): save login in SignInForm

--- front/src/components/forms/SignInForm.js
+++ front/src/components/forms/SignInForm.js
@@ -1,10 +1,53 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { toast } from "react-toastify";
 import Button from '../Button';
 import Input from '../inputs/Input';
+import { useValidationForms } from '../../hooks/useValidationForms';
+import { useAuth } from '../../contexts/AuthContextApiHook';
 
 export default function SignInForm() {
+    const { validateRequiredFields, validateEmailField } = useValidationForms();
+    const navigate = useNavigate();
+    const { sign } = useAuth();
+
+    const onSubmit = async (e) => {
+        e.preventDefault();
+        console.log('data: ', e.target.elements);
+
+        const email = e.target.elements['email'].value;
+        const password = e.target.elements['password'].value;
+
+        let validationErrors = validateRequiredFields([
+            { input: email, msg: 'O campo e-mail é obrigatório' },
+            { input: password, msg: 'O campo senha é obrigatório' },
+        ]);
+
+        validationErrors += validateEmailField(email);
+
+        if (validationErrors === 0) {
+            const formData = new FormData();
+            formData.append('email', email);
+            formData.append('password', password);
+
+            try {
+                const response = await sign({ email, password });
+                console.log('response: ', response);
+
+                if (!response.message) {
+                    toast.success(`Olá, ${response.name}!`);
+                    navigate('/inicio');
+                } else {
+                    toast.error(response.message);
+                }
+            } catch (error) {
+                console.error('Error:', error);
+                toast.error('Erro no cadastro!');
+            }
+        }
+    }
+
     return (
-        <form class="space-y-4 md:space-y-6" action="#">
+        <form className="space-y-4 md:space-y-6" method="post" onSubmit={onSubmit}>
             <Input
                 type="email"
                 name="email"
