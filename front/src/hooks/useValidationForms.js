4b3110d3fcbe21059cdcc994de559c55f31ee6be
MicaelRiboura <micaelribouradossantos@gmail.com>
2023-11-29 10:26:51 -0300

    feat(front): create useValidationForm

---
+++ front/src/hooks/useValidationForms.js
@@ -0,0 +1,41 @@
+import { toast } from "react-toastify";
+
+export const useValidationForms = () => {
+    const validateRequiredFields = (fields) => {
+        let validationErrors = 0;
+        fields.forEach(({ input, msg }) => {
+            if (input === '') {
+                toast.error(msg);
+                validationErrors++;
+            }
+        });
+
+        return validationErrors;
+    }
+
+    const validateEmailField = (field) => {
+        let validationErrors = 0;
+        if (!field.includes('@')) {
+            toast.error('O campo e-mail está incorreto');
+            validationErrors++;
+        }
+
+        return validationErrors;
+    }
+
+    const validateResourceField = (field) => {
+        let validationErrors = 0;
+        if (!field.includes('https://')) {
+            toast.error('O campo recurso precisa ser um link válido!');
+            validationErrors++;
+        }
+
+        return validationErrors;
+    }
+
+    return {
+        validateRequiredFields,
+        validateEmailField,
+        validateResourceField,
+    }
+}
