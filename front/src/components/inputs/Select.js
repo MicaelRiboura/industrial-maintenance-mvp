9887c51afbc73d548f6e8972b3f553ef9b8897b8
MicaelRiboura <micaelribouradossantos@gmail.com>
2023-11-29 12:06:50 -0300

    fix(front): adjust key in Select component

--- front/src/components/inputs/Select.js
+++ front/src/components/inputs/Select.js
@@ -7,8 +7,8 @@ export default function Select({ name, label, groupClassName, options }) {
                 name={name}
                 className="w-full bg-dark-800 text-white px-3 py-2 mb-1 border-2 border-dark-800 rounded-md focus:outline-none focus:border-primary-500 transition-colors"
             >
-               {options.map(option => (
-                    <option value={option.value}>{option.label}</option>
+               {options.map((option, index) => (
+                    <option key={`${name}-${index}`} value={option.value}>{option.label}</option>
                 ))}
             </select>
         </div>
